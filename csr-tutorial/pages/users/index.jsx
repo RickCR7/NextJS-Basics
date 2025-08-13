8c6ed621873f555e37fb29c7849fca7abe10c84a
Akash Sen <iconicrick@gmail.com>
2025-08-13 10:53:48 +0530

    Modular css and image component in nextJs

--- csr-tutorial/pages/users/index.jsx
+++ csr-tutorial/pages/users/index.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import useSWR from "swr";
 
+import styles from "../../styles/User.module.css"
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const UserPage = () => {
@@ -13,12 +15,12 @@ const UserPage = () => {
   if (!data) return <h1>Loading...</h1>;
 
   return (
-    <div>
+    <div >
       <h1>Users</h1>
       {data.users &&
         data.users.map((user) => (
           <Link href={`/users/${user.id}`} key={user.id}>
-            <div>{user.firstName}</div>
+            <div className={styles.text}>{user.firstName}</div>
           </Link>
         ))}
     </div>
